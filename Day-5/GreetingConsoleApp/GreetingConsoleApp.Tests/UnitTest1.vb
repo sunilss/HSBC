9f794e60f249dbe8e0420f4f1425b652c9b035fe
Magesh <tkmagesh77@gmail.com>
2013-07-12 11:29:50 +0530

    Unit Testing

---
+++ Day-5/GreetingConsoleApp/GreetingConsoleApp.Tests/UnitTest1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Text
+
+Public Class FakeDateTimeServiceReturningMorningTime
+    Implements IDateService
+
+    Public Function GetCurrentDateTime() As Date Implements IDateService.GetCurrentDateTime
+        Return New DateTime(2013, 7, 12, 9, 0, 0)
+    End Function
+End Class
+
+Public Class FakeDateTimeServiceReturningEveningTime
+    Implements IDateService
+
+    Public Function GetCurrentDateTime() As Date Implements IDateService.GetCurrentDateTime
+        Return New DateTime(2013, 7, 12, 19, 0, 0)
+    End Function
+End Class
+
+
+<TestClass()>
+Public Class GreeterTests
+
+    <TestMethod()>
+    Public Sub When_Greeted_Before_18_Greets_Good_Day()
+        'Arrange
+        Dim dateTimeService = New FakeDateTimeServiceReturningMorningTime
+        Dim sut = New Greeter(dateTimeService)
+        Dim name = "magesh"
+
+        'Act
+        Dim response = sut.Greet(name)
+
+        'Assert
+        Assert.IsTrue(response.StartsWith("Good Day"))
+    End Sub
+
+    <TestMethod()>
+    Public Sub When_Greeted_After_18_Greets_Good_Evening()
+        'Arrange
+        Dim dateTimeService = New FakeDateTimeServiceReturningEveningTime
+        Dim sut = New Greeter(dateTimeService)
+        Dim name = "magesh"
+
+        'Act
+        Dim response = sut.Greet(name)
+
+        'Assert
+        Assert.IsTrue(response.StartsWith("Good Evening"))
+    End Sub
+
+End Class
---
+++ Day-5/GreetingConsoleApp/GreetingConsoleApp/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Enter your name : ")
+        Dim name = Console.ReadLine()
+        Dim dService = New DateService()
+        Dim greeter = New Greeter(dService)
+        Console.WriteLine(greeter.Greet(name))
+        Console.ReadLine()
+    End Sub
+
+End Module
+
+Public Class Greeter
+    Dim dService As IDateService
+    Public Sub New(ByVal dService As IDateService)
+        Me.dService = dService
+    End Sub
+
+    Public Function Greet(ByVal name As String) As String
+        If (Me.dService.GetCurrentDateTime.Hour < 16) Then
+            Return String.Format("Good Day {0}", name)
+        Else
+            Return String.Format("Good Evening {0}", name)
+        End If
+    End Function
+End Class
+
+Public Interface IDateService
+    Function GetCurrentDateTime() As DateTime
+End Interface
+
+Public Class DateService
+    Implements IDateService
+
+    Public Function GetCurrentDateTime1() As Date Implements IDateService.GetCurrentDateTime
+        Return DateAndTime.Now
+    End Function
+End Class
