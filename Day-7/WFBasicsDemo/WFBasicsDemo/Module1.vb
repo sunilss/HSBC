137096005f63da0a5640d5607ae79d9f3f8ce72e
Magesh <tkmagesh77@gmail.com>
2013-07-14 16:58:44 +0530

    Final

---
+++ Day-7/WFBasicsDemo/WFBasicsDemo/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Activities
+Imports System.Activities.Statements
+Imports System.Diagnostics
+Imports System.Linq
+
+Module Module1
+
+    Dim s As Sequence
+
+    Sub Main()
+        Dim args As New Dictionary(Of String, Object)
+        args.Add("name", "Magesh")
+        WorkflowInvoker.Invoke(New Workflow1(), args)
+
+    End Sub
+
+End Module
